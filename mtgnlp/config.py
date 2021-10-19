c4aae18567a855901f747f3efd8d281eeaa6cc3c
pedrovgp <pedrovgp@gmail.com>
2021-10-19 10:27:32 -0300

    MOVE unsensitive info to config instead of env

--- mtgnlp/config.py
+++ mtgnlp/config.py
@@ -3,8 +3,17 @@
 # Append the below to venv/bin/activate and your .env variables will be loaded on activation (opening a new terminal)
 # export $(grep -v '^#' .env | xargs)
 import os
+from pathlib import Path
+import mtgnlp
 
 DB_STR = os.getenv("DB_STR")
-DATA_DIR = "data"
-CARD_IMAGES_DIR = "card_images"
-GRAPHS_DIR = "card_images"
+ROOT_DIR = Path(mtgnlp.__file__).parent.parent
+DATA_DIR = ROOT_DIR.joinpath("data")
+CARD_IMAGES_DIR = ROOT_DIR.joinpath("card_images")
+GRAPHS_DIR = ROOT_DIR.joinpath("graphs")
+MTGMETA_DECK_TNAME = "mtgmetaio_decks"
+MTGMETA_CARDS_TNAME = "mtgmetaio_cards_in_deck"
+CARDS_TNAME = "cards"
+DECKS_TNAME = "decks"
+DECKS_FEATURES_TNAME = "deck_features"
+ALL_PRINTINGS_PATH = DATA_DIR.joinpath("AllPrintings20211017.json")
